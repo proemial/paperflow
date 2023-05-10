8c438b17aff2502c41c572f00ab5109a4ba5c395
Brian <brian@visikon.com>
2023-05-10 14:58:31 +0200

    cleanup

--- apps/ingestion/app/api/scheduler/tick/route.ts
+++ apps/ingestion/app/api/scheduler/tick/route.ts
@@ -14,29 +14,40 @@ export async function GET() {
   try {
     const date = dayjs().format("YYYY-MM-DD");
 
-    const ids = await fetchUpdatedIds(date);
+    const output = await fetchLatestPapers(date);
 
-    let ingestionState = await IngestionDao.getOrCreate(date);
+    // TODO: push to qStash (limit by category)
 
-    const newIds = ids.filter(id => !ingestionState.ids.hits.includes(id) && !ingestionState.ids.misses.includes(id));
-
-    const limit = DateFactory.yesterday();
-    const output = await fetchUpdatedItems(newIds, limit);
-
-    if (output.misses.length > 0 || output.hits.length > 0) {
-      if (output.hits.length > 0) {
-        await DocsDao.upsertMany(output.hits);
+    return NextResponse.json({
+      papers: {
+        updated: output.hits.length,
+        ignored: output.misses.length
       }
-
-      ingestionState.ids.hits.push(...output.hits.map(item => item.parsed.id));
-      ingestionState.ids.misses.push(...output.misses.map(item => item.parsed.id));
-      await IngestionDao.update(date, ingestionState);
-    }
-
-    // TODO: push to qStash
-
-    return NextResponse.json({ updated: output.hits.length, ignored: output.misses.length });
+    });
   } finally {
     console.log(`[${DateMetrics.elapsed(begin)}] /api/scheduler/tick`);
   }
+}
+
+async function fetchLatestPapers(date: string) {
+  const ids = await fetchUpdatedIds(date);
+
+  let ingestionState = await IngestionDao.getOrCreate(date);
+
+  const newIds = ids.filter(id => !ingestionState.ids.hits.includes(id) && !ingestionState.ids.misses.includes(id));
+
+  const limit = DateFactory.yesterday();
+  const output = await fetchUpdatedItems(newIds, limit);
+
+  if (output.misses.length > 0 || output.hits.length > 0) {
+    if (output.hits.length > 0) {
+      await DocsDao.upsertMany(output.hits);
+    }
+
+    ingestionState.ids.hits.push(...output.hits.map(item => item.parsed.id));
+    ingestionState.ids.misses.push(...output.misses.map(item => item.parsed.id));
+    await IngestionDao.update(date, ingestionState);
+  }
+
+  return output;
 }
