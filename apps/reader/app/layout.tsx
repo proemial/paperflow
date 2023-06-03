2d65e0298aea88c8dc022e7f076b46fd61c2a146
Paperflow Lab <lab@paperflow.ai>
2023-06-03 17:13:04 +0200

    use dvh to counter for iOS issues

--- apps/reader/app/layout.tsx
+++ apps/reader/app/layout.tsx
@@ -17,7 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="flex min-h-screen max-h-screen flex-col md:flex-row-reverse justify-between">
+        <main className="flex min-h-screen max-h-screen flex-col md:flex-row-reverse justify-between" style={{
+          minHeight: '100dvh',
+          maxHeight: '100dvh',
+          }}>
           <div className="flex-1 p-2 overflow-scroll">{children}</div>
           <div className="border-zinc-300 max-sm:shadow-inner md:border-r md:h-screen sticky top-0">
             <Menu />
