113ec838cd7f6a8513ecc4b17beef5b75c4d1532
Paperflow Lab <lab@paperflow.ai>
2023-06-03 10:11:24 +0200

    Add breakpoints

--- apps/reader/app/(pages)/(home)/page.tsx
+++ apps/reader/app/(pages)/(home)/page.tsx
@@ -1,9 +1,11 @@
 
 export default function Home() {
   return (
-    <main>
-      <div>content</div>
-      <div>menu</div>
+    <main className="flex min-h-screen flex-col-reverse md:flex-row justify-between">
+      <div className="border-2 border-orange-500">MENU / SHARE / SIGN-UP</div>
+      <div className="border-2 flex-1 border-green-500">
+        CONTENT
+      </div>
     </main>
   )
 }
