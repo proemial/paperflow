1aa29fcb0a81e1ccf5d3d0003d55156df83e4763
Paperflow Lab <lab@paperflow.ai>
2023-06-05 06:48:51 +0200

    spinner

--- apps/reader/app/(pages)/(home)/loading.tsx
+++ apps/reader/app/(pages)/(home)/loading.tsx
@@ -1,3 +1,9 @@
+import Spinner from "@/app/components/spinner";
+
 export default function Loading() {
-  return <div>Loading ...</div>;
+  return (
+    <div className="h-[90dvh] flex justify-center items-center">
+      <Spinner />
+    </div>
+  );
 }
