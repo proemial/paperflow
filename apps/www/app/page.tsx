8c7419446aeab20daa0b1a66712d42a968c4cf6f
Paperflow Lab <lab@paperflow.ai>
2023-06-07 16:31:41 +0200

    landing page

---
+++ apps/www/app/page.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white dark:invert-[1]">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      </div>
+
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2">
+        <Image
+          className="relative"
+          src="/logo.png"
+          alt="Paperflow Logo"
+          width={582}
+          height={252}
+          priority
+        />
+      </div>
+
+      
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+      </div>
+    </main>
+  )
+}
