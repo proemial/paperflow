250b53a6eebaf77dd4c68e86986e127cb346d453
Paperflow Lab <lab@paperflow.ai>
2023-06-11 12:05:13 +0200

    Set timet zone in ingestion logger to utc +2

--- packages/data/storage/ingestion-log.ts
+++ packages/data/storage/ingestion-log.ts
@@ -1,13 +1,17 @@
 import dayjs from "dayjs";
 import { UpStash } from "../adapters/redis/upstash-client";
 
+// @ts-ignore
+import dayjsPluginUTC from 'dayjs-plugin-utc'
+dayjs.extend(dayjsPluginUTC)
+
 export const IngestionLogger = {
     log: async (date: string, msg: string) => {
       console.log(msg);
 
       await UpStash.ingestionLog.append(
         date,
-        `[${dayjs().format("DD.MM.YYYY HH:mm:ss")}] ${msg} \n`
+        `[${dayjs().utcOffset(120).format("DD.MM.YYYY HH:mm:ss")}] ${msg} \n`
       )
     },
 };
