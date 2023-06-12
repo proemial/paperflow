62779094b8401c3bc788266c4d73f103265dd5ae
Paperflow Lab <lab@paperflow.ai>
2023-06-12 06:59:53 +0200

    improve skeleton card

--- apps/reader/app/components/paperflow-card/skeleton.tsx
+++ apps/reader/app/components/paperflow-card/skeleton.tsx
@@ -1,5 +1,13 @@
+import clsx from "clsx";
 import { Card } from "../shadcn-ui/Card";
+import Spinner from "../spinner";
 
 export function SkeletonCard() {
-    return <Card className="max-sm:w-full h-12 before:animate-[shimmer_1.5s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/10 before:to-transparent"></Card>
+    //
+    return <Card className={clsx('max-sm:w-full flex justify-center p-4', {
+        'before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_1.5s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/10 before:to-transparent':
+          true,
+      })}>
+        <Spinner />
+      </Card>
 }
