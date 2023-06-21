99e065a47c676ef922dc8a73e58e963ece24caaf
Paperflow Lab <lab@paperflow.ai>
2023-06-21 05:34:22 +0200

    add logo

--- apps/reader/app/components/menu.tsx
+++ apps/reader/app/components/menu.tsx
@@ -1,14 +1,19 @@
 "use client";
+import Link from "next/link";
 import { Toaster } from "./shadcn-ui/Toaster";
 import { ShareButton } from "./share-button";
+import Image from "next/image";
 
 export function Menu() {
   return (
     <>
-    {/* border: none;
+      {/* border: none;
     padding: 0;
     height: 24px; */}
-      <div className="flex justify-end p-2 h-full items-end">
+      <div className="flex p-2 h-full items-end justify-between md:flex-col">
+        <Link href="/">
+          <Image src="/logo.png" height={40} width={40} alt="" />
+        </Link>
         <ShareButton />
       </div>
       <Toaster />
