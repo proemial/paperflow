e9121e716f792a7e6b8e3464b9c5e79316ac86ad
Paperflow Lab <lab@paperflow.ai>
2023-06-23 07:32:42 +0200

    use local logo

--- apps/reader/app/components/menu.tsx
+++ apps/reader/app/components/menu.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Toaster } from "./shadcn-ui/Toaster";
 import { ShareButton } from "./share-button";
 import Image from "next/image";
+import logo from "./logo.png";
 
 export function Menu() {
   return (
@@ -12,7 +13,7 @@ export function Menu() {
     height: 24px; */}
       <div className="flex p-2 h-full items-end justify-between md:flex-col">
         <Link href="/">
-          <Image src="/logo.png" height={40} width={40} alt="" />
+          <Image src={logo} height={40} width={40} alt="" />
         </Link>
         <ShareButton />
       </div>
