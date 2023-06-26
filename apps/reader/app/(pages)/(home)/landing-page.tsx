ae28604b206bf0db6947ef04b660645807f6dd33
Paperflow Lab <lab@paperflow.ai>
2023-06-27 00:09:50 +0200

    invert logo in dark mode

--- apps/reader/app/(pages)/(home)/landing-page.tsx
+++ apps/reader/app/(pages)/(home)/landing-page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export function LandingPage() {
   return (
-    <div className="flex m-2 h-full items-center justify-center">
+    <div className="flex m-2 h-full items-center justify-center dark:invert-[1]">
       <Image
         src={logo}
         alt="Paperflow Logo"
