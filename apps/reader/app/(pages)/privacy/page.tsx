bd30fb9128862a2e7c1ebde8c977ae36dbc5e66d
Paperflow Lab <lab@paperflow.ai>
2023-07-11 06:00:26 +0200

    Add privacy

---
+++ apps/reader/app/(pages)/privacy/page.tsx
@@ -0,0 +1,54 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white dark:invert-[1]">
+      <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2">
+        <div>
+          <h1>PRIVACY POLICY</h1>
+          <p>Effective Date: June 27, 2023</p>
+          <p>
+            Paperflow, a company located at [Company Address], along with its
+            affiliates and subsidiaries (collectively, &quot;Paperflow,&quot;
+            &quot;we,&quot; &quot;our,&quot; or &quot;us&quot;), respects your
+            privacy. This Privacy Policy describes our approach towards personal
+            data in relation to our website, www.paperflow.ai (the
+            &quot;Site&quot;) and the services available through our Site (the
+            &quot;Services&quot;).
+          </p>
+          <h2>INFORMATION WE COLLECT</h2>
+          <p>
+            We, at Paperflow, have designed our systems and Services in a way
+            that minimizes data collection. When you visit or use our Site and
+            Services, we do not collect any personal data such as names, email
+            addresses, phone numbers, mailing addresses, billing information,
+            usage data, device and browser data, or information from cookies and
+            similar technologies.
+          </p>
+          <h2>HOW WE PROTECT YOUR INFORMATION</h2>
+          <p>
+            We prioritize the security of our users. Although we do not collect
+            personal data, we maintain robust security measures to protect our
+            Site and Services from unauthorized access, alteration, disclosure,
+            or destruction.
+          </p>
+          <h2>THIRD-PARTY SERVICES</h2>
+          <p>
+            Our Site and Services may contain links to third-party websites or
+            services. We are not responsible for the privacy practices or
+            content of these third parties. We encourage you to read the privacy
+            policies of any third-party websites or services you visit.
+          </p>
+          <h2>CHANGES TO THIS POLICY</h2>
+          <p>
+            We may update this Privacy Policy from time to time. If we make any
+            changes, we will post the updated policy on this page with a new
+            effective date. We encourage you to review this Privacy Policy
+            periodically to stay informed about our practices. CONTACT US If you
+            have any questions or concerns about this Privacy Policy, please
+            contact us at [Contact Email]. By using our Site and Services, you
+            acknowledge that you have read and understood this Privacy Policy.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
