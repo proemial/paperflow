50abb5e6ef775a2a5097bc18ae680bbf91983f08
Paperflow Lab <lab@paperflow.ai>
2023-07-12 11:04:37 +0200

    Format paper card

--- apps/reader/src/components/paper-card.tsx
+++ apps/reader/src/components/paper-card.tsx
@@ -1,19 +1,25 @@
 import assetImg from "./images/asset-bg-1.png";
+import scrollImg from "./images/scroll.svg";
 
 export function PaperCard() {
   console.log("assetImg", assetImg.src);
 
   return (
     <div
-      className="min-h-[256px] px-4 py-8"
+      className="min-h-[256px] px-4 py-8 flex flex-col justify-center items-center"
       style={{
         backgroundImage: `url(${assetImg.src}) no-repeat`,
         backgroundSize: "cover",
       }}
     >
-      DR-TB patients in Bangladesh face delays due to seeking care from multiple
-      informal providers
-      <div className="flex justify-end">Microsummary by Paperflow </div>
+      <div className="text-2xl font-light">
+        DR-TB patients in Bangladesh face delays due to seeking care from
+        multiple informal providers
+      </div>
+      <div className="w-full pt-4 text-sm font-light flex justify-end items-center">
+        <img src={scrollImg.src} width={16} alt="" />
+        Microsummary by Paperflow{" "}
+      </div>
     </div>
   );
 }
