306a0f2d12f88eb4ddb08eddaf1fc831c7444bc1
Paperflow Lab <lab@paperflow.ai>
2023-07-12 06:39:55 +0200

    Add menu icons

--- apps/reader/src/components/menu.tsx
+++ apps/reader/src/components/menu.tsx
@@ -1,10 +1,20 @@
+import { Home, Search, Bookmark, KeyRound } from "lucide-react";
+
 export function MainMenu() {
   return (
     <div className="flex justify-around">
-      <div>Ho</div>
-      <div>Se</div>
-      <div>Sa</div>
-      <div>Pr</div>
+      <div>
+        <Home />
+      </div>
+      <div>
+        <Search />
+      </div>
+      <div>
+        <Bookmark />
+      </div>
+      <div>
+        <KeyRound />
+      </div>
     </div>
   );
 }
