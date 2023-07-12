a8110f9538332543bc44d00ba9d363472e6d5a05
Paperflow Lab <lab@paperflow.ai>
2023-07-12 21:41:58 +0200

    Fetch live data for landing page

--- apps/reader/src/app/page.tsx
+++ apps/reader/src/app/page.tsx
@@ -1,42 +1,31 @@
+import { PipelineDao } from "data/storage/pipeline";
 import { PaperCard } from "src/components/card";
+import { Suspense } from "react";
 
-const papers = [
-  {
-    id: "2307.04986",
-    text: `Artificial intelligence-powered agents mimic real-world behaviors, flatten epidemic curves, and model complex dynamic systems.`,
-    category: "Artificial Intelligence",
-    tags: ["#AI", "#EpidemicModeling", "#BehaviorSimulation"],
-    published: "2023-07-11",
-  },
-  {
-    id: "2307.04964",
-    text: `New advanced version of PPO algorithm improves stability and training of large language models, revolutionizing AI development.`,
-    category: "Computation and Language",
-    tags: ["#LLMs", "#PPO", "#AI"],
-    published: "2023-07-11",
-  },
-  {
-    id: "2307.05402",
-    text: "Graphs without long paths are hard to analyze. Matching Cut problem complexity increases. Exponential Time Hypothesis holds strong.",
-    category: "Computational Complexity",
-    tags: ["#Graphs", "#Matching", "#Exponential"],
-    published: "2023-07-11",
-  },
-  {
-    id: "2307.05061",
-    text: `Maximizing social welfare in score-based social distance games is computationally tractable on tree-like networks.`,
-    category: "Computer Science and Game Theory",
-    tags: ["#SocialWelfare", "#CoalitionFormation", "#GameTheory"],
-    published: "2023-07-11",
-  },
-];
+export default async function Home() {
+  const ingestionIndex = await PipelineDao.getIngestionIndex();
+  const index = await PipelineDao.getIndex(ingestionIndex.at(-1));
 
-export default function Home() {
+  const latestIds = index
+    .map((entry) => entry.id)
+    .sort()
+    .reverse();
+  const randomIds = getMultipleRandom(latestIds, 20);
+
   return (
     <main className="flex min-h-screen flex-col justify-start">
-      {papers.map((paper, index) => (
-        <PaperCard key={index} paper={paper} />
+      {randomIds.map((id, index) => (
+        <Suspense key={index} fallback={<div>S</div>}>
+          {/* @ts-expect-error Server Component */}
+          <PaperCard key={index} id={id} />
+        </Suspense>
       ))}
     </main>
   );
+}
+
+function getMultipleRandom(arr: string[], num: number) {
+  const shuffled = [...arr].sort(() => 0.5 - Math.random());
+
+  return shuffled.slice(0, num);
 }
--- apps/reader/src/components/card.tsx
+++ apps/reader/src/components/card.tsx
@@ -1,34 +1,29 @@
 import assetImg1 from "src/images/asset-bg-1.png";
 import assetImg2 from "src/images/asset-bg-2.png";
 import assetImg3 from "src/images/asset-bg-3.png";
-import { Heart } from "lucide-react";
-import { CardLink } from "./card-link";
 import { Badge } from "./badge";
+import { CardLink } from "./card-link";
+import { PapersDao } from "data/storage/papers";
+import { sanitize } from "./sanitizer";
 
 const images = [assetImg1, assetImg2, assetImg3];
 
-type Props = {
-  paper: {
-    id: string;
-    text: string;
-    category: string;
-    tags: string[];
-    published: string;
-  };
-};
+export async function PaperCard({ id }: { id: string }) {
+  const { text } = await PapersDao.getGptSummary(id, "sm");
+  const paper = await PapersDao.getArXivAtomPaper(id);
+  const sanitized = sanitize(text);
 
-export function PaperCard({ paper }: Props) {
   return (
     <div
       className="p-4 pt-8 flex flex-col justify-end items-center shadow-[inset_0_-48px_48px_rgba(0,0,0,0.9)]"
       style={{
-        backgroundImage: `url(${image(paper.id)})`,
+        backgroundImage: `url(${image(id)})`,
         backgroundSize: "cover",
       }}
     >
-      <CardLink>{paper.text}</CardLink>
+      <CardLink>{sanitized.sanitized}</CardLink>
       <div className="w-full pt-6 text-xs flex justify-begin gap-2 overflow-scroll no-scrollbar">
-        {paper.tags.map((tag, index) => (
+        {sanitized.hashtags.map((tag, index) => (
           <Badge key={index} text={tag.slice(1)} />
         ))}
       </div>
