0d73f1f87f4a6ce72ced017823e323636cfc639f
Paperflow Lab <lab@paperflow.ai>
2023-07-13 17:04:14 +0200

    badge colors

--- apps/reader/src/components/badge.tsx
+++ apps/reader/src/components/badge.tsx
@@ -5,12 +5,13 @@ import { useState } from "react";
 export function Badge({ text }: { text: string }) {
   const [liked, setLiked] = useState(false);
 
-  const textStyle = liked ? "bg-white text-black" : "bg-black";
-  const heartStyle = liked ? "fill-black" : "";
+  const borderStyle = liked ? "" : "border-primary";
+  const textStyle = liked ? "bg-white" : "bg-black";
+  const heartStyle = liked ? "fill-primary" : "";
 
   return (
     <div
-      className={`${textStyle} border rounded-lg px-1 py-0 whitespace-nowrap flex items-center gap-1`}
+      className={`${textStyle} ${borderStyle} text-purple-500 border rounded-lg px-1 py-0 whitespace-nowrap flex items-center gap-1`}
       onClick={() => setLiked(!liked)}
     >
       <Heart width={16} className={heartStyle} />
--- apps/reader/src/components/card-content.tsx
+++ apps/reader/src/components/card-content.tsx
@@ -15,11 +15,10 @@ export function CardContent({ id, text, tags }: Props) {
   };
 
   return (
-    <div
-      onClick={handleClick}
-      className="p-4 pt-8 flex flex-col justify-end text-lg font-medium items-center "
-    >
-      <Markdown>{text}</Markdown>
+    <div className="p-4 pt-8 flex flex-col justify-end text-lg font-medium items-center ">
+      <div onClick={handleClick}>
+        <Markdown>{text}</Markdown>
+      </div>
       <div className="w-full pt-6 text-xs font-medium tracking-wider flex justify-begin gap-2 overflow-scroll no-scrollbar">
         {tags.map((tag, index) => (
           <Badge key={index} text={tag.slice(1)} />
