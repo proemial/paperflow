8c4d6e11ee8df4d1a67fce10bfea5f22b0168ce7
Paperflow Lab <lab@paperflow.ai>
2023-07-16 04:46:56 +0200

    Change toaster color and timeout

--- apps/reader/src/components/shadcn-ui/toaster.tsx
+++ apps/reader/src/components/shadcn-ui/toaster.tsx
@@ -14,7 +14,7 @@ export function Toaster() {
   const { toasts } = useToast();
 
   return (
-    <ToastProvider duration={1800}>
+    <ToastProvider duration={2600}>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
           <Toast key={id} {...props}>
