6588a7b4e04fb630575476dc8ec93c366626f09c
Paperflow Lab <lab@paperflow.ai>
2023-07-21 16:31:26 +0200

    fix landing page logo centering

--- apps/reader/src/app/page.tsx
+++ apps/reader/src/app/page.tsx
@@ -27,7 +27,7 @@ export default function HomePage() {
   return (
     <QueryClientProvider client={queryClient}>
       {!user && (
-        <main className="flex min-h-screen flex-col justify-center items-center">
+        <main className="flex min-h-full flex-col justify-center items-center">
           <img src={logo.src} width="50%" />
           <div className="text-4xl md:text-7xl">paperflow</div>
         </main>
