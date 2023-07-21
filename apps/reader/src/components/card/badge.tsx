8d94ee3c09052fd44c508bcda5b69a6241dab8e0
Paperflow Lab <lab@paperflow.ai>
2023-07-21 06:25:58 +0200

    fix logged out issue

--- apps/reader/src/components/card/badge.tsx
+++ apps/reader/src/components/card/badge.tsx
@@ -3,6 +3,7 @@ import { Heart } from "lucide-react";
 import { useState, useTransition } from "react";
 import { like } from "./card-actions";
 import { Analytics } from "../analytics";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 type Props = {
   id: string;
@@ -12,10 +13,14 @@ type Props = {
 };
 
 export function Badge({ id, category, text, likes }: Props) {
+  const { user } = useUser();
   const [checked, setChecked] = useState(likes?.includes(text));
   const [isPending, startTransition] = useTransition();
 
   const handleClick = () => {
+    if (!user) {
+      return;
+    }
     setChecked(!checked);
     Analytics.track(!!checked ? "click:like-clear" : "click:like", {
       id,
@@ -29,13 +34,23 @@ export function Badge({ id, category, text, likes }: Props) {
     startTransition(() => like(id, category, updatedLikes));
   };
 
-  const borderStyle = checked ? "" : "border-primary";
-  const textStyle = checked ? "bg-white" : "bg-black";
-  const heartStyle = checked ? "fill-primary" : "";
+  let textStyle = checked
+    ? "bg-white text-purple-500"
+    : "bg-black text-purple-500";
+  let borderStyle = checked ? "" : "border-primary";
+  let heartStyle = checked ? "fill-primary" : "";
+  let cursorStyle = "cursor-pointer";
+
+  if (!user) {
+    textStyle = "bg-black text-zinc-600";
+    heartStyle = "fill-zinc-600";
+    borderStyle = "border-zinc-600";
+    cursorStyle = "";
+  }
 
   return (
     <div
-      className={`${textStyle} ${borderStyle} text-purple-500 border rounded-lg px-1 py-0 whitespace-nowrap flex items-center gap-1 cursor-pointer`}
+      className={`${textStyle} ${borderStyle} ${cursorStyle} text-purple-500 border rounded-lg px-1 py-0 whitespace-nowrap flex items-center gap-1`}
       onClick={handleClick}
     >
       <Heart width={16} className={heartStyle} />
