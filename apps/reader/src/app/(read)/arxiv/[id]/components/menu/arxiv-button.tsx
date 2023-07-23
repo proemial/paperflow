fe5e2945184c15092f145bab926b698f03f321da
Paperflow Lab <lab@paperflow.ai>
2023-07-23 21:57:59 +0200

    open arxiv in new window

--- apps/reader/src/app/(read)/arxiv/[id]/components/menu/arxiv-button.tsx
+++ apps/reader/src/app/(read)/arxiv/[id]/components/menu/arxiv-button.tsx
@@ -3,10 +3,13 @@ import { Analytics } from "src/components/analytics";
 import { Button } from "src/components/shadcn-ui/button";
 
 export function ArxivButton({ id }: { id: string }) {
-  const handleClick = () => {
-    Analytics.track("click:share", { id });
-    window.location.href = `https://arxiv.org/abs/${id}`;
-  };
-
-  return <Button onClick={() => handleClick()}>Read the full article</Button>;
+  return (
+    <a
+      href={`https://arxiv.org/abs/${id}`}
+      target="_blank"
+      className="rounded-md bg-gradient-to-r from-primary to-primary-gradient text-primary-foreground hover:from-primary/90 hover:to-primary-gradient/90 text-base py-2 px-4 h-10 flex items-center"
+    >
+      Read the full article
+    </a>
+  );
 }
