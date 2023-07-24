aef6e7d87867bf62b881700e80e43d4b826ca35e
Paperflow Lab <lab@paperflow.ai>
2023-07-24 19:08:03 +0200

    more related papers

--- apps/reader/src/app/(read)/arxiv/[id]/components/related-research.tsx
+++ apps/reader/src/app/(read)/arxiv/[id]/components/related-research.tsx
@@ -2,6 +2,7 @@ import { PaperCard } from "src/components/card/card";
 import { Suspense } from "react";
 import { PipelineDao } from "data/storage/pipeline";
 import { EmptySpinner } from "src/components/spinner";
+import { arXivCategory } from "data/adapters/arxiv/arxiv.models";
 
 type Props = {
   id: string;
@@ -11,13 +12,24 @@ type Props = {
 export async function RelatedResearch({ id, category }: Props) {
   const index = await PipelineDao.getIngestionIndex();
   const ingested = await PipelineDao.getIndex(index?.at(-1));
-  const filtered = ingested?.filter(
-    (entry) => entry.id !== id && entry.category === category
+
+  const paperCategory = arXivCategory(category);
+  const filtered = ingested
+    ?.filter((e) => e.id !== id)
+    .map((e) => ({ id: e.id, ...arXivCategory(e.category) }))
+    .filter((e) => e.category === paperCategory.category);
+
+  const sameCategory = filtered.filter((e) => e.key === paperCategory.key);
+  const sameTopic = filtered.filter(
+    (e) => e.category === paperCategory.category && !sameCategory.includes(e)
   );
+
+  const finalSet = [...sameCategory, ...sameTopic];
+  console.log(finalSet);
 
   return (
     <>
-      {filtered?.slice(0, 3).map((entry, i) => (
+      {finalSet?.slice(0, 10).map((entry, i) => (
         <div key={i}>
           <Suspense fallback={<EmptySpinner />}>
             {/* @ts-expect-error Server Component */}
