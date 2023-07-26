48c59accb61bbafab605ae304ee251c00f71709a
Paperflow Lab <lab@paperflow.ai>
2023-07-26 11:17:50 +0200

    update beta button

--- apps/reader/src/app/beta/page.tsx
+++ apps/reader/src/app/beta/page.tsx
@@ -16,15 +16,15 @@ export default function BetaPage() {
   }, [user]);
 
   return (
-    <main className="flex min-h-full flex-col justify-center items-center">
+    <main className="flex min-h-screen flex-col justify-center items-center">
       <img src={logo.src} width="50%" />
       <div className="text-4xl md:text-7xl">paperflow</div>
       <Button
         variant="secondary"
         className="mt-8"
-        onClick={() => (window.location.href = `/api/auth/login`)}
+        onClick={() => (window.location.href = `/?reload=true`)}
       >
-        Try Now
+        Click here for access
       </Button>
     </main>
   );
