e6eb99b6a45f9ba7d7ef7eca2af12b4aa6fa6068
Paperflow Lab <lab@paperflow.ai>
2023-08-12 16:31:46 +0200

    Lunk to pdf

--- apps/reader/src/app/(read)/arxiv/[id]/components/menu/arxiv-button.tsx
+++ apps/reader/src/app/(read)/arxiv/[id]/components/menu/arxiv-button.tsx
@@ -5,7 +5,7 @@ import { Button } from "src/components/shadcn-ui/button";
 export function ArxivButton({ id }: { id: string }) {
   return (
     <a
-      href={`https://arxiv.org/abs/${id}`}
+      href={`https://arxiv.org/pdf/${id}`}
       target="_blank"
       className="rounded-md bg-gradient-to-r from-primary to-primary-gradient text-primary-foreground hover:from-primary/90 hover:to-primary-gradient/90 text-base py-2 px-4 h-10 flex items-center"
     >
