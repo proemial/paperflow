a21af0d57a7fc973a2ff9c6b2a974ab89079f369
Paperflow Lab <lab@paperflow.ai>
2023-08-22 14:01:55 +0200

    tag manager preparations

--- apps/reader/src/components/analytics.tsx
+++ apps/reader/src/components/analytics.tsx
@@ -35,6 +35,28 @@ export function AnalyticsClient() {
   return <VercelAnalytics />;
 }
 
+/*
+
+// Paste this code as high in the <head> of the page as possible:
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WWZ6WDTF');</script>
+<!-- End Google Tag Manager -->
+
+
+// Additionally, paste this code immediately after the opening <body> tag:
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WWZ6WDTF"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+ */
+
 export const Analytics = {
   track: (event: string, properties?: Record<string, any>) => {
     va.track(event, properties);
@@ -50,6 +72,7 @@ function useGoogleAnalytics() {
 
   useEffect(() => {
     if (user) {
+      // const email = user["https://paperflow.ai/email"] as string;
       ReactGA.initialize("G-2H4D1N8XGN", {
         gaOptions: {
           userId: user.sub,
