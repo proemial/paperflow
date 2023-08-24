ae89d5450f027fe1ef4746fdb41d087ce5e6467e
Paperflow Lab <lab@paperflow.ai>
2023-08-24 10:10:19 +0200

    Log metadata ingestion result

--- apps/ingestion/app/api/workers/metadata/metadata.ts
+++ apps/ingestion/app/api/workers/metadata/metadata.ts
@@ -1,12 +1,17 @@
+import { IngestionLogger } from "data/storage/ingestion-log";
 import { PapersDao } from "data/storage/papers";
 import { PipelineDao } from "data/storage/pipeline";
+import { DateMetrics } from "utils/date";
 
 export async function extractMetadata(date: string) {
     const index = await PipelineDao.getIndex(date);
+    const begin = DateMetrics.now();
 
     if(index.length > 0){
         const metadata = await PapersDao.buildMetadata(index.map((i) => i.id));
         await PipelineDao.pushMetadata(date, metadata);
+
+        await IngestionLogger.log(date, `[metadata][${DateMetrics.elapsed(begin)}] indexLength: ${index.length}, metadata length: ${Object.keys(metadata).length}`);
     }
 
     return {date, count: index.length};
