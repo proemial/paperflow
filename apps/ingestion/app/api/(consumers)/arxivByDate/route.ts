1aac1f757132f3495331c730c2bff1e56101f329
Paperflow Lab <lab@paperflow.ai>
2023-11-03 06:32:35 +0100

    try again, vercel didn't deploy the last one

--- apps/ingestion/app/api/(consumers)/arxivByDate/route.ts
+++ apps/ingestion/app/api/(consumers)/arxivByDate/route.ts
@@ -1,6 +1,7 @@
 import {arxivByDate} from "./worker";
 
 export async function POST(request: Request) {
+    console.log(`POST /api/arxivByDate`)
     const payload = await request.text();
     return await arxivByDate(payload);
 }
