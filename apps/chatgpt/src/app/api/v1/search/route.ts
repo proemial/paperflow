b0b461b9631db37f79aa5729465aaf196fd7a1aa
Paperflow Lab <lab@paperflow.ai>
2023-11-14 15:06:37 +0100

    Add chatgpt app

---
+++ apps/chatgpt/src/app/api/v1/search/route.ts
@@ -0,0 +1,11 @@
+import {NextResponse} from "next/server";
+import {specificPapers} from "../data";
+
+export const runtime = 'edge';
+
+export async function POST(req: Request) {
+    const body = await req.json();
+    console.log('api/search', body);
+
+    return NextResponse.json(specificPapers);
+}
